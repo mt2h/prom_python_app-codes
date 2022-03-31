e95c6f06e7ac28ecf936da6761121aab08226497
momo <momo@your.domain.com>
2022-03-31 04:45:13 +0000

    more examples for gauge

--- gauge.py
+++ gauge.py
@@ -13,18 +13,19 @@ class HandleRequests(http.server.BaseHTTPRequestHandler):
 
     @REQUEST_INPROGRESS.track_inprogress()
     def do_GET(self):
-       # REQUEST_INPROGRESS.inc()
+        REQUEST_INPROGRESS.inc()
         time.sleep(5)
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>First Application</title></head><body style='color: #333; margin-top: 30px;'><center><h2>Welcome to our first Prometheus-Python application.</center></h2></body></html>", "utf-8"))
-        self.wfile.close()
+        #self.wfile.close()
         REQUEST_LAST_SERVED.set_to_current_time()
-       # REQUEST_LAST_SERVED.set(time.time())
-       #REQUEST_INPROGRESS.dec()
+        REQUEST_LAST_SERVED.set(time.time())
+        REQUEST_INPROGRESS.dec()
 
 if __name__ == "__main__":
     start_http_server(METRICS_PORT)
-    server = http.server.HTTPServer(('localhost', APP_PORT), HandleRequests)
+    #server = http.server.HTTPServer(('localhost', APP_PORT), HandleRequests)
+    server = http.server.HTTPServer(('0.0.0.0', APP_PORT), HandleRequests)
     server.serve_forever()
